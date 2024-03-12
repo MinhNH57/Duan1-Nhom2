68c9edadf3b4cd2883ad5f109cd4880d08c478c7
MinhNH57 <nutmsotn04@gmail.com>
2024-03-12 20:52:50 +0700

    NguyenHongMinh_Update 6

--- Dự án 1/obj/Debug/net8.0-windows/Dự án 1.AssemblyInfo.cs
+++ Dự án 1/obj/Debug/net8.0-windows/Dự án 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dự án 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8948d657fac4a6acfc16d94587750fdf61f76d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37505af414c07f9f7481646d07dcd0cc91d32848")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dự án 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dự án 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
